bb8f59b35e369427e77fcc62423688c989f2bdfc
blenassefa2 <blenassefa59@yahoo.com>
2023-12-19 22:57:04 +0300

    folder restructure and new edits

--- Part_1/Exercise/bot/bot_instance.py
+++ Part_1/Exercise/bot/bot_instance.py
@@ -6,6 +6,14 @@ from config import TOKEN_API
 # from dotenv import load_dotenv
 # load_dotenv()
 
+# For hosting on pythonanywhere use the following commented code
+# from aiogram.client.session.aiohttp import AiohttpSession
+# bot = Bot(
+#     token=TOKEN_API,
+#     parse_mode='HTML',
+#     session=AiohttpSession(proxy='http://proxy.server:3128')
+# )
+
 bot = Bot(
     token=TOKEN_API,
     parse_mode='HTML'
